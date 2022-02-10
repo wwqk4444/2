8c9d391847440567f3136de5802b9b85dcae0b64
zhengying <y_zhengok@163.com>
2022-02-11 01:35:58 +0800

    update

--- build_linux.sh
+++ build_linux.sh
@@ -1,2 +1,1 @@
-GOOS=linux GOARCH=amd64 go build ./server -o bin/kcpraw-server-amd64-linux  
-GOOS=linux GOARCH=amd64 go build ./client -o bin/kcpraw-client-amd64-linux  
+GOOS=linux GOARCH=amd64 go build ./server -o bin/kcpraw_server_linux_amd64
